56299c9ff0bad4642abc3c77078b7f1b5252bf4b
MK <michael.keck89@yahoo.com>
2023-12-12 20:27:31 +0100

    day 11, part 2

--- aoc2023_11.py
+++ aoc2023_11.py
@@ -51,54 +51,98 @@ def solve(data, part2=False):
       arr_framed[-1,c+1] = '^'
    [print(''.join(l)) for l in arr_framed]; print()
 
-   # new dims: H + len(rows_no_galax), W + len(cols_no_galax)
-   H_new, W_new = H + len(rows_no_galax), W + len(cols_no_galax)
-   arr_expanded = np.chararray((H_new,W_new), unicode=True)
-   arr_expanded[:] = '~'
-   [print(''.join(l)) for l in arr_expanded]; print()
+   if part2:
+      y_list, x_list = np.where(arr == '#')
+      coords = [[y,x] for y,x in zip(y_list, x_list)]
+      print("coords:", coords)
+      N = len(coords)
+      N_pairs = (N*(N-1))//2 # N choose 2
+      print(f"N pairs: {N_pairs}")
 
-   #expand_arr_rows(arr_expanded, arr, rows_no_galax, cols_no_galax):
-   offset = 0
-   i = 0
-   while i < H_new:
-      print("i",i)
-      if i in np.array(rows_no_galax) + offset:
-         arr_expanded[[i,i+1],:-len(cols_no_galax)] = arr[i-offset,:]
+      print("rows_no_galax:", rows_no_galax)
+      print("cols_no_galax:", cols_no_galax)
+      expansion_value = 1000000 # 2 -> 374, 10 -> 1030, 100 -> 8410, 1000000 -> 790194712336
+      offset = expansion_value-1
+      print("expansion_value, offset:", expansion_value, offset)
+      
+      coords = sorted(coords, key=lambda x: x[0]) # sort by y
+      print("coords sorted by y:", coords)
+      y_offsets = np.zeros(len(coords), dtype=int)
+      for r in rows_no_galax:
+         for i in range(len(coords)):
+            coord_y = coords[i][0]
+            if coord_y > r:
+               y_offsets[i] += offset
+      for i in range(len(coords)):
+         coords[i][0] += y_offsets[i]
+      print("coords after going row-by-row:", coords)
+      
+      coords = sorted(coords, key=lambda x: x[1]) # sort by x
+      print("coords sorted by x:", coords)
+      x_offsets = np.zeros(len(coords), dtype=int)
+      for c in cols_no_galax:
+         for i in range(len(coords)):
+            coord_x = coords[i][1]
+            if coord_x > c:
+               x_offsets[i] += offset
+      for i in range(len(coords)):
+         coords[i][1] += x_offsets[i]
+      print("coords after going col-by-col:", coords)
+
+      shortest_paths = []
+      for i in range(len(coords)):
+         for j in range(i+1,len(coords)):
+            d = np.sum(np.abs(np.array(coords[i]) - np.array(coords[j])))
+            shortest_paths.append(d)
+      res = np.sum(shortest_paths)
+
+   else:
+      # new dims: H + len(rows_no_galax), W + len(cols_no_galax)
+      H_new, W_new = H + len(rows_no_galax), W + len(cols_no_galax)
+      arr_expanded = np.chararray((H_new,W_new), unicode=True)
+      arr_expanded[:] = '~'
+      [print(''.join(l)) for l in arr_expanded]; print()
+
+      #expand_arr_rows(arr_expanded, arr, rows_no_galax, cols_no_galax):
+      offset = 0
+      i = 0
+      while i < H_new:
+         print("i",i)
+         if i in np.array(rows_no_galax) + offset:
+            arr_expanded[[i,i+1],:-len(cols_no_galax)] = arr[i-offset,:]
+            i += 1
+            offset += 1
+         else:
+            arr_expanded[i,:-len(cols_no_galax)] = arr[i-offset,:]
          i += 1
-         offset += 1
-      else:
-         arr_expanded[i,:-len(cols_no_galax)] = arr[i-offset,:]
-      i += 1
-   [print(''.join(l)) for l in arr_expanded]; print()
+      [print(''.join(l)) for l in arr_expanded]; print()
 
-   #expand_arr_rows(arr_expanded.T, arr.T, cols_no_galax):
-   offset = 0
-   i = 0
-   arr = deepcopy(arr_expanded)
-   while i < W_new:
-      print("i",i)
-      if i in np.array(cols_no_galax) + offset:
-         arr_expanded.T[[i,i+1],:] = arr.T[i-offset,:]
+      #expand_arr_rows(arr_expanded.T, arr.T, cols_no_galax):
+      offset = 0
+      i = 0
+      arr = deepcopy(arr_expanded)
+      while i < W_new:
+         if i in np.array(cols_no_galax) + offset:
+            arr_expanded.T[[i,i+1],:] = arr.T[i-offset,:]
+            i += 1
+            offset += 1
+         else:
+            arr_expanded.T[i,:] = arr.T[i-offset,:]
          i += 1
-         offset += 1
-      else:
-         arr_expanded.T[i,:] = arr.T[i-offset,:]
-      i += 1
-   [print(''.join(l)) for l in arr_expanded]; print()
+      [print(''.join(l)) for l in arr_expanded]; print()
 
-   x_list, y_list = np.where(arr_expanded == '#')
-   coords = [[x,y] for x,y in zip(x_list, y_list)]
-   print(coords)
-   N = len(coords)
-   N_pairs = (N*(N-1))//2 # N choose 2
-   print(f"N pairs: {N_pairs}")
-   shortest_paths = []
-   for i in range(N):
-      for j in range(i+1,N):
-         d = np.sum(np.abs(np.array(coords[i]) - np.array(coords[j])))
-         shortest_paths.append(d)
-
-   res = np.sum(shortest_paths)
+      x_list, y_list = np.where(arr_expanded == '#')
+      coords = [[x,y] for x,y in zip(x_list, y_list)]
+      print(coords)
+      N = len(coords)
+      N_pairs = (N*(N-1))//2 # N choose 2
+      print(f"N pairs: {N_pairs}")
+      shortest_paths = []
+      for i in range(N):
+         for j in range(i+1,N):
+            d = np.sum(np.abs(np.array(coords[i]) - np.array(coords[j])))
+            shortest_paths.append(d)
+      res = np.sum(shortest_paths)
 
    if part2:
       print("_"*60)
@@ -109,13 +153,13 @@ def solve(data, part2=False):
 if __name__ == "__main__":
    #––– sample input
    print("for sample input:")
-   solve(sample_input, part2=False)
-   #solve(sample_input, part2=True)
+   #solve(sample_input, part2=False)
+   solve(sample_input, part2=True)
    #exit()
 
    #––– Part 1 and Part 2
    print("for input file:")
    with open('input_11.txt', 'r') as f:
       file_data = f.readlines()
-      solve(file_data, part2=False)
-      #solve(file_data, part2=True)
+      #solve(file_data, part2=False)
+      solve(file_data, part2=True)
